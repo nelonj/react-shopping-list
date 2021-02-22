275ae3f163f8b2f1bf733a5abe3050d67c7121a7
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-22 15:20:38 +0100

    basic structure working (2 input bars for item and amount; a list gets printed every time i click the add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
+
+let myItemList: string[] = []
+let cloneList: string[] = []
+let printList: JSX.Element[] = []
+let myAmountList: string[] = []
+
+
 function App() {
+  const [item, setItem] = useState("")
+  const [amount, setAmount] = useState("")
+  const [list, setList] = useState([''])
+  console.log('hello')
+  
+  
+
+  function clickHandler() {
+    
+    return (
+    cloneList = [...myItemList],  
+    myItemList.push(item),
+    myAmountList.push(amount.toString()),
+    printList = myItemList.map((element, index) => <li key={index}> {`${element} : ${myAmountList[index]}`} </li>),
+    setList(cloneList)
+    )
+  }
+//  let printList = myList.map((element, index) => <li key={index}> {`${element} : ${amount}`} </li>)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My Shopping List </h1>
+      
+      <div className="input-bar">
+      <input value ={item} onChange={(event) => setItem(event.target.value)} /> 
+      <input value ={amount} onChange={event => setAmount(event.target.value)} />
+      </div>
+      
+      <div className="register-button">
+        <button onClick={clickHandler}> add! </button>
+       
+      </div>
+     
+     
+      <div className="shopping-list">
+        <ul> 
+          {printList}
+        </ul>
+      </div>
+      
+      
     </div>
   );
 }
