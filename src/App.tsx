ee2733d26f07ad031c3ce0a756dbe9f115b39440
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-23 12:25:02 +0100

    refactored: renders an empty shopping list to start with

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,78 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { setConstantValue } from 'typescript';
 import './App.css';
 
 
 
-let myItemList: string[] = []
-let cloneList: string[] = []
-let printList: JSX.Element[] = []
-let myAmountList: string[] = []
+// let myItemList: { item: string, amount: string }[] = [{ item: "", amount: '' }]
+// let cloneList: { item: string, amount: string }[] = [{ item: "", amount: '' }]
 
+interface Item {
+  item: string,
+  amount: string
+}
+type List = Item[]
 
 function App() {
   const [item, setItem] = useState("")
   const [amount, setAmount] = useState("")
-  const [list, setList] = useState([''])
+  let startingList: List = []
+  const [list, setList] = useState(startingList)
   console.log('hello')
-  
+
   function removeItem(id: number) {
-    
+
+    // return (
+    //   cloneList = [...myItemList],
+    //   myItemList = myItemList.filter((element, index) => index !== id),
+    //   setList(cloneList)
+    // )
+  }
+
+  function ShoppingList() {
     return (
-    cloneList = [...myItemList],
-    myItemList = myItemList.filter((element, index) => index !== id ),
-    myAmountList = myAmountList.filter((element, index) => index !== id ),
-    printList = myItemList.map((element, index) => 
-      <li key={index}> {`${element} : ${myAmountList[index]}`} 
-      <button className='delete-button' onClick={() => removeItem(index)} > X </button> 
-      </li>),
-    setList(cloneList)
+      <ul>
+        {list.map((element, index) =>
+          <li key={index}> {`${element.item} : ${element.amount}`}
+            <button className='delete-button' onClick={() => removeItem(index)} > X </button>
+          </li>)}
+      </ul>
     )
   }
 
   function clickHandler() {
-    
-    return (
-    cloneList = [...myItemList],  
-    myItemList.push(item),
-    myAmountList.push(amount.toString()),
-    printList = myItemList.map((element, index) => 
-      <li key={index}> {`${element} : ${myAmountList[index]}`} 
-      <button className='delete-button' onClick={() => removeItem(index)} > X </button> 
-      </li>),
-    setList(cloneList)
-    )
+
+    // return (
+    //   cloneList = [...myItemList],
+    //   myItemList.push({ item: item, amount: amount }),
+    //   setList(cloneList)
+    // )
   }
-//  let printList = myList.map((element, index) => <li key={index}> {`${element} : ${amount}`} </li>)
+
   
+
   return (
     <div className="App">
       <h1> My Shopping List </h1>
-      
+
       <div className="input-bar">
-      <input value ={item} onChange={(event) => setItem(event.target.value)} /> 
-      <input value ={amount} onChange={event => setAmount(event.target.value)} />
+        <input value={item} onChange={(event) => setItem(event.target.value)} />
+        <input value={amount} onChange={event => setAmount(event.target.value)} />
       </div>
-      
+
       <div className="register-button">
         <button onClick={clickHandler}> add! </button>
-       
+
       </div>
-     
-     
+
+
       <div className="shopping-list">
-        <ul> 
-          {printList}
+        <ul>
+          <ShoppingList />
         </ul>
       </div>
-      
-      
+
+
     </div>
   );
 }
